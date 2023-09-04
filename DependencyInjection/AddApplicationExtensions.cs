67183f87af5bb78d3eccfbb375adeb0294b23900
Poostoi <oleg.nour@bk.ru>
2023-09-04 12:37:41 +0300

    feat: all dependencies have been removed

---
+++ DependencyInjection/AddApplicationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services;
+using Services.Services;
+
+namespace DependencyInjection;
+
+public static partial class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection service)
+    {
+        service.AddScoped<IUserService, UserService>();
+        service.AddScoped<IProjectService, ProjectService>();
+        service.AddScoped<ISprintService, SprintService>();
+        service.AddScoped<ITaskService, TaskService>();
+        return service;
+    }
+}
---
+++ DependencyInjection/AddInfrastructureExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Providers;
+using Providers.Providers;
+using Services.Providers;
+
+namespace DependencyInjection;
+
+public static partial class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection service)
+    {
+        service.AddDbContext<ApplicationContext>();
+        service.AddCors(o => o.AddPolicy("ReactPolicy", builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        }));
+        service.AddScoped<IUserProvider, UserProvider>();
+        service.AddScoped<IRoleProvider, RoleProvider>();
+        service.AddScoped<IProjectProvider, ProjectProvider>();
+        service.AddScoped<ISprintProvider, SprintProvider>();
+        service.AddScoped<ITaskProvider, TaskProvider>();
+
+
+        return service;
+    }
+}
